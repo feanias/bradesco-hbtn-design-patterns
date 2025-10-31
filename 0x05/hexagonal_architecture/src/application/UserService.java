c92e8eb87ae92641d9f1c7ae429eb7e3fd1057d4
Felipe Anias <36409468+feanias@users.noreply.github.com>
2025-10-30 22:04:48 -0300

    Exercicio 1

---
+++ 0x05/hexagonal_architecture/src/application/UserService.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.List;
+
+import ports.UserRepository;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void createUser(String id, String name) {
+        //implemente o metodo
+    }
+
+
+    public User getUser(String id) {
+        //implemente o metodo
+    }
+
+
+    public List<User> listUsers() {
+        return null;
+    }
+}
